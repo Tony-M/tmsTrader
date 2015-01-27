f5c7b574468956fb51961889f60791cb64af6639
Морозов Антон Андреевич <aamorozov83@gmail.com>
2015-01-27 18:29:37 +0300

    Изменил маршрут в свой бандл

---
+++ app/cache/dev/twig/15/0f/60f3dc4301fc9d357bf7cc704c6b1b4e0f8a34756619253389963bb5bc4f.php
@@ -0,0 +1,32 @@
+<?php
+
+/* TmsTraderBundle:Default:index.html.twig */
+class __TwigTemplate_150f60f3dc4301fc9d357bf7cc704c6b1b4e0f8a34756619253389963bb5bc4f extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "TmsTraderBundle:Default:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
